362c13f9c31dd91d0a78e335d7c0274f9149d38e
BrunoSette <brunosette@gmail.com>
2019-07-12 17:44:25 -0300

    change some file names

--- components/enviaBanco.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const shell = require("shelljs");
-
-
-  shell.exec(
-          "mongoimport --host "+process.env.DB_HOST+" --ssl --username m220student --password "+process.env.DB_PASS+" --authenticationDatabase admin --db serverless --collection notes --type json --file recife.json --jsonArray --mode merge"
-        );
-
--- components/google-trends.js
+++ components/google-trends.js
@@ -36,5 +36,4 @@ setTimeout(function() {
       fileName +
       " --mode merge"
   );
-  //shell.exec('mongoimport --host mflix-shard-0/mflix-shard-00-00-p6kx5.mongodb.net:27017,mflix-shard-00-01-p6kx5.mongodb.net:27017,mflix-shard-00-02-p6kx5.mongodb.net:27017 --ssl --username m220student --password m220password --authenticationDatabase admin --db mflix --collection googleTrends --type json --file '+ fileName + ' --jsonArray --mode merge')
 }, 3000);
