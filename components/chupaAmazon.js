78508573f359379dd32e3dd49d787edc566287ad
BrunoSette <brunosette@gmail.com>
2019-07-18 08:54:26 -0300

    Change Eslint to standard

--- components/chupaAmazon.js
+++ components/chupaAmazon.js
@@ -1,48 +1,48 @@
-const Xray = require("x-ray");
-const pages = 1; // Config Number of Pages to Scrapp at once
-const fileName = "./../results/eletronics-amazon.json";
+const Xray = require('x-ray')
+const pages = 1 // Config Number of Pages to Scrapp at once
+const fileName = './../results/eletronics-amazon.json'
 
 const x = Xray({
   filters: {
-    trim: function(value) {
-      return typeof value === "string" ? value.trim() : value;
+    trim: function (value) {
+      return typeof value === 'string' ? value.trim() : value
     },
-    reverse: function(value) {
-      return typeof value === "string"
+    reverse: function (value) {
+      return typeof value === 'string'
         ? value
-            .split("")
-            .reverse()
-            .join("")
-        : value;
+          .split('')
+          .reverse()
+          .join('')
+        : value
     },
-    slice: function(value, start, end) {
-      return typeof value === "string" ? value.slice(start, end) : value;
+    slice: function (value, start, end) {
+      return typeof value === 'string' ? value.slice(start, end) : value
     }
   }
-});
+})
 
 x(
-  "https://www.amazon.com/s?i=electronics-intl-ship&rh=n%3A%2116225009011&page=2",
-  ".s-result-item",
+  'https://www.amazon.com/s?i=electronics-intl-ship&rh=n%3A%2116225009011&page=2',
+  '.s-result-item',
   [
     {
-      asin: "@data-asin",
-      description: ".a-size-medium",
-      link: "a@href",
-      details: x("a@href", {
-        bestsellersrank: ".a-color-secondary", // no
-        dimensions: ".a-size-base", // no
+      asin: '@data-asin',
+      description: '.a-size-medium',
+      link: 'a@href',
+      details: x('a@href', {
+        bestsellersrank: '.a-color-secondary', // no
+        dimensions: '.a-size-base', // no
 
-        releasedate: "tbody > tr > tr > td.a-size-base"
+        releasedate: 'tbody > tr > tr > td.a-size-base'
       }),
-      image: "img@src",
-      price: ".a-price-whole",
-      cents: ".a-price-fraction",
-      rating: ".a-icon-alt | slice:0,3",
-      votes: ".a-size-base"
+      image: 'img@src',
+      price: '.a-price-whole',
+      cents: '.a-price-fraction',
+      rating: '.a-icon-alt | slice:0,3',
+      votes: '.a-size-base'
     }
   ]
 )
-  .paginate("li.a-last > a@href")
+  .paginate('li.a-last > a@href')
   .limit(pages)
-  .write(fileName);
+  .write(fileName)
--- components/chupaCabra.js
+++ components/chupaCabra.js
@@ -1,65 +1,65 @@
-const Xray = require("x-ray");
-const pages = 1; // TODO Jogar esses dados em arquivo de Config
-const fileName = "./../results/cars-olx.json"; // nome do arquivo para exportar
-const brand = "";
-const model = "";
-const year = "";
+const Xray = require('x-ray')
+const pages = 1 // TODO Jogar esses dados em arquivo de Config
+const fileName = './../results/cars-olx.json' // nome do arquivo para exportar
+const brand = ''
+const model = ''
+const year = ''
 
 const x = Xray({
   filters: {
-    trim: function(value) {
-      return typeof value === "string" ? value.trim() : value;
+    trim: function (value) {
+      return typeof value === 'string' ? value.trim() : value
     },
-    reverse: function(value) {
-      return typeof value === "string"
+    reverse: function (value) {
+      return typeof value === 'string'
         ? value
-            .split("")
-            .reverse()
-            .join("")
-        : value;
+          .split('')
+          .reverse()
+          .join('')
+        : value
     },
-    slice: function(value, start, end) {
-      return typeof value === "string" ? value.slice(start, end) : value;
+    slice: function (value, start, end) {
+      return typeof value === 'string' ? value.slice(start, end) : value
     }
   }
-});
+})
 
-const mountUrl = (brand = "", model = "", year = "") => {
+const mountUrl = (brand = '', model = '', year = '') => {
   let baseUrl =
-    "https://pe.olx.com.br/grande-recife/recife/autos-e-pecas/carros-vans-e-utilitarios/";
-  if (brand !== "") {
-    baseUrl += `${brand}/`;
-    if (model !== "") {
-      baseUrl += `${model}/`;
+    'https://pe.olx.com.br/grande-recife/recife/autos-e-pecas/carros-vans-e-utilitarios/'
+  if (brand !== '') {
+    baseUrl += `${brand}/`
+    if (model !== '') {
+      baseUrl += `${model}/`
     }
   }
 
-  if (year !== "") {
-    baseUrl += `${year}`;
+  if (year !== '') {
+    baseUrl += `${year}`
   }
 
-  return baseUrl;
-};
+  return baseUrl
+}
 
-x(mountUrl(brand, model, year), ".item", [
+x(mountUrl(brand, model, year), '.item', [
   {
-    id: "a@data-lurker_list_id",
-    description: "img@alt",
-    link: "a[lurker=list_id]@href",
-    next_page: x("a[lurker=list_id]@href", {
-      categoria: ".title@h4", // no
-      modelo: "a[class=link]@title", // no
-      data: "tbody > tr > tr > td.a-size-base",
-      opcionais: "ul .OLXad-features-list"
+    id: 'a@data-lurker_list_id',
+    description: 'img@alt',
+    link: 'a[lurker=list_id]@href',
+    next_page: x('a[lurker=list_id]@href', {
+      categoria: '.title@h4', // no
+      modelo: 'a[class=link]@title', // no
+      data: 'tbody > tr > tr > td.a-size-base',
+      opcionais: 'ul .OLXad-features-list'
     }),
-    image: "img@data-original",
-    price: ".OLXad-list-price | slice:4",
-    details: ".detail-specific",
-    city: ".detail-region | slice:10,16",
+    image: 'img@data-original',
+    price: '.OLXad-list-price | slice:4',
+    details: '.detail-specific',
+    city: '.detail-region | slice:10,16',
     bairro:
-      ".detail-region | slice:29,60 | trim:(\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t)"
+      '.detail-region | slice:29,60 | trim:(\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t)'
   }
 ])
-  .paginate("a[next]@href")
+  .paginate('a[next]@href')
   .limit(pages)
-  .write(fileName);
+  .write(fileName)
